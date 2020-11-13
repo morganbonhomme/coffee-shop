34f41948dd4c8a1a3f66b67880ee3b7f3763c3bd
morganbonhomme <bonhomme.morgan@gmail.com>
2020-11-13 12:16:45 +0100

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyACozVbbE1WBkQM9e38iJWEJTlizmXh54A",
+    authDomain: "coffeeshop-c91d7.firebaseapp.com",
+    databaseURL: "https://coffeeshop-c91d7.firebaseio.com",
+    projectId: "coffeeshop-c91d7",
+    storageBucket: "coffeeshop-c91d7.appspot.com",
+    messagingSenderId: "318414409584",
+    appId: "1:318414409584:web:cd82295674f2d15d9bc9b5"
+  }
 };
+
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyACozVbbE1WBkQM9e38iJWEJTlizmXh54A",
+    authDomain: "coffeeshop-c91d7.firebaseapp.com",
+    databaseURL: "https://coffeeshop-c91d7.firebaseio.com",
+    projectId: "coffeeshop-c91d7",
+    storageBucket: "coffeeshop-c91d7.appspot.com",
+    messagingSenderId: "318414409584",
+    appId: "1:318414409584:web:cd82295674f2d15d9bc9b5"
+  }
 };
 
 /*
